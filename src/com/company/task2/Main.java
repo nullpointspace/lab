e90ae1f16d0d1c5728700343dcdc8576a31eb708
maxim <git@nullpoint.space>
2020-10-16 22:54:48 +0300

    add class

--- src/com/company/task2/Main.java
+++ src/com/company/task2/Main.java
@@ -2,10 +2,11 @@ package com.company.task2;
 
 public class Main {
     public static void main(String[] args) {
-        Animal[] animals = new Animal[3];
-        animals[0] = new Cat("food1", "location1");
-        animals[1] = new Dog("food2", "location2");
-        animals[2] = new Horse("food3", "location3");
+        Animal[] animals = new Animal[]{
+            new Cat("food1", "location1"),
+            new Dog("food2", "location2"),
+            new Horse("food3", "location3")
+        };
 
         Veterinarian veterinarian = new Veterinarian();
         for (Animal animal : animals) {
